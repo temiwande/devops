67c3b72c23a15b8474e86b93d1659042d58683b8
temiwande <103294875+temiwande@users.noreply.github.com>
2022-04-08 22:35:04 -0400

    Update script.sh
    made some changes

--- script.sh
+++ script.sh
@@ -7,3 +7,16 @@ sleep 2
 echo
 echo
 echo "Would you like me to go ahead and create a personal user for you?"
+read ans
+#write an if statement if the ans is yes continue if the answer is no quit
+echo "Thanks for using my services"
+echo
+echo
+sleep 3
+echo "Which one of the following would you like me to do?"
+echo "1. Create a user"
+echo "2. Create a group"
+echo "3. Create a file"
+echo "4. Create a folder"
+echo "5. Create a webserver"
+Write a case statement that will perform any action you want the user to do
