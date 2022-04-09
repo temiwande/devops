c46ce380211b9e0bef2f387fddc63cd825380c90
root <temiwandeesho@gmail.com>
2022-04-09 02:21:00 +0000

    First trial

---
+++ Readme.md
@@ -0,0 +1,5 @@
+This is a read me file
+
+The script in the repo is to create users and do some other task
+You can run this using "bash script.sh"
+Thank you
---
+++ script.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Welcome to scripting lifestyle"
+sleep 5
+echo "If you like I can create a user with a folder for you"
+sleep 2
+echo
+echo
+echo "Would you like me to go ahead and create a personal user for you?"
